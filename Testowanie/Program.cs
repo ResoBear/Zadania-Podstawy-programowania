f030d73763ce7780fe0aefdd88a6e6adba5b59d4
ResoBear <resobear@gmail.com>
2018-10-20 17:30:03 +0200

    update 1.1

--- Testowanie/Program.cs
+++ Testowanie/Program.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace Testowanie
 {
-    class Program
+
+    namespace _01.Decimal_to_Binary
     {
-
-        static void Main(string[] args)
+        class DecimalToBinary
         {
+            static void Main(string[] args)
+            {
+                Console.Write("Decimal: ");
+                int decimalNumber = int.Parse(Console.ReadLine());
 
+                int remainder;
+                string result = ("");
+                while (decimalNumber > 0)
+                {
+                    remainder = decimalNumber % 2;
+                    decimalNumber /= 2;
+                    result = remainder.ToString() + result;
+                }
+                Console.WriteLine("Binary:  {0}", result);
+            }
         }
-
     }
 }
---
+++ Zadanie6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zadanie6
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program konwertuje liczbę dziesiętną na binarną.");
+            Console.Write("Podaj liczbę całkowitą dodatnią: ");
+            int decimalNumber = int.Parse(Console.ReadLine());
+
+            int remainder;
+            string result = ("");
+            while (decimalNumber > 0)
+            {
+                remainder = decimalNumber % 2;
+                decimalNumber /= 2;
+                result = remainder.ToString() + result;
+            }
+            Console.WriteLine("Postać binarna:  {0}", result);
+        }
+    }
+}
